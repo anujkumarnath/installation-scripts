66b6db261d89edadca77301bb57300251a9f09ea
Anuj <anuj@antares-tech.com>
2023-02-18 16:26:00 +0000

    Sourcing bashrc file at the end now to make things work

--- starship/install.sh
+++ starship/install.sh
@@ -5,15 +5,17 @@ rm -rf ~/.config/starship.toml
 # Removing customization git repo if any to avoid renaming
 rm ~/starship.toml
 
-# Downloading and nstalling latest version of Starship 
+# Downloading and installing latest version of Starship 
 curl -sS https://starship.rs/install.sh | sh
 
-# Updating bashrc and sourcing it
+# Updating bashrc
 ( echo ""; echo 'eval "$(starship init bash)"' ) >> ~/.bashrc
-source ~/.bashrc
 
 # Downloading my own set of customizations
 wget https://github.com/anujkumarnath/starship-custom-config/raw/master/starship.toml
 
 # Moving the file to ensure that it works with Starship
 mv starship.toml ~/.config/
+
+# Sourcing bashrc to get things working
+source ~/.bashrc
